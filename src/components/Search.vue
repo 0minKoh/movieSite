bee2fa0687d0a8bf841d18a4dbd0ccb134311b77
0minKoh <esesang13@naver.com>
2022-04-01 16:52:01 +0900

    ~search

---
+++ src/components/Search.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="container">
+    <input
+      v-model="title"
+      class="form-control"
+      type="text"
+      placeholder="Search for Movies, Series & more"
+      @keyup.enter="apply" />
+    <div class="selects">
+      <!-- $전역속성 [] = .인데, 반응성 있게 만듬-->
+      <select
+        v-for="filter in filters"
+        v-model="$data[filter.name]" 
+        :key="filter.name"
+        class="form-select"> 
+        <!-- All years를 year에만 적용하기 v-if -->
+        <option
+          v-if="filter.name === 'year'" 
+          value="">
+          All Years
+        </option>
+        <option
+          v-for="item in filter.items"
+          :key="item">
+          {{ item }}
+        </option>
+      </select>
+    </div>
+    <button
+      class="btn btn-primary"
+      @click="apply">
+      Apply
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '',
+      type: 'movie',
+      number: 10,
+      year: '',
+      filters: [
+        {
+          name: 'type',
+          items: ['movie', 'series', 'episode']
+        },
+        {
+          name: 'number',
+          items: [10, 20, 30]
+        },
+        {
+          name: 'year',
+          items: (() => {
+            const years = []
+            const thisYear = new Date().getFullYear()
+            for (let i = thisYear; i >= 1985; i -= 1) {
+              years.push(i)
+            }
+            return years
+          })()
+        }
+      ]
+    }
+  },
+  methods: {
+    apply() {
+      this.$store.dispatch('movie/searchMovies', {
+        title: this.title,
+        type: this.type,
+        number: this.number,
+        year: this.year
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  > * { //* 전체 선택자
+    margin-right: 10px;
+    font-size: 15px;
+    &:last-child { //예외적으로, 마지막 요소는 ....
+      margin-right: 0;
+    }
+  }
+  .selects {
+    display: flex;
+    select {
+      width: 120px;
+      margin-right: 10px;
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+  }
+  .btn {
+    width: 120px;
+    height: 50px;
+    flex-shrink: 0; //감소 비율 없음 (120px 너비 유지)
+    font-weight: 700;
+  }
+
+  @include media-breakpoint-down(lg) {
+    display: block;
+    input {
+      margin-right: 0;
+      margin-bottom: 10px;
+    }
+    .selects {
+      margin-right: 0;
+      margin-bottom: 10px;
+      select {
+        width: 100%;
+      }
+    }
+    .btn {
+      width: 100%;
+    }
+  }
+}
+</style>
